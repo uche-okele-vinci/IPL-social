a26f79b832978824289731ffd8832d7a673f7d01
uche okele <uche.okele@student.vinci.be>
2025-01-17 17:21:16 +0100

    Commit test number 4

--- passwordChecker.test.js
+++ passwordChecker.test.js
@@ -12,4 +12,10 @@ describe('Password Validation', () => {
   it('should return false for passwords without a digit', () => {
     expect(isPasswordValid('abcdefgh!')).toBe(false);
   });
+
+  it('should return false for passwords containing "IPL" in any case', () => {
+    expect(isPasswordValid('abcdeIPL1!')).toBe(false);
+    expect(isPasswordValid('abcdeipl1!')).toBe(false);
+    expect(isPasswordValid('abcdeIpl1!')).toBe(false);
+  });
 });
