0fadc628d90221741a9da336fa24abdf5f9b09e6
uche okele <uche.okele@student.vinci.be>
2025-01-17 17:18:45 +0100

    Commit for test number 2

--- passwordChecker.test.js
+++ passwordChecker.test.js
@@ -4,4 +4,8 @@ describe('Password Validation', () => {
   it('should return false for passwords with less than 8 characters', () => {
     expect(isPasswordValid('abc!1')).toBe(false);
   });
+
+  it('should return false for passwords without a special character', () => {
+    expect(isPasswordValid('abcdefgh1')).toBe(false);
+  });
 });
